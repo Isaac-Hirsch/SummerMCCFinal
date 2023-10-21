3bda61aa4358ec757c3464eae7cfba49e244e775
Isaac Hirsch <isaac@Isaacs-MacBook-Air-2.local>
2023-10-21 16:04:43 -0500

    finished first iteration of file that should take in truth files and use the MCParticle_SiTracks_Refitted collection to find a delta phi and delta theta

---
+++ doubletFiltering/October2023/truthLevelAnalysis.py
@@ -0,0 +1,117 @@
+import numpy as np
+import glob
+from optparse import OptionParser
+import json
+from pyLCIO import IOIMPL, EVENT, UTIL
+
+#Gather all the files you want to run over.
+#Comment out all but 1 fnames
+#0-50 pt muons
+fnames, outName = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_0_50/muonGun_pT_0_50_reco_*.slcio"), "deltaThetaPhi0NoBIBTruth"
+#50-250 pt muons
+#fnames, outName = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_0_50/muonGun_pT_50_250_reco_*.slcio"), "deltaThetaPhi50NoBIBTruth"
+#250-1000 pt muons
+#fnames, outName = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_250_1000/muonGun_pT_250_1000_reco_*.slcio"), "deltaThetaPhi250NoBIBTruth"
+
+parser = OptionParser()
+parser.add_option('-i', '--inFile', help='--inFile Output_REC.slcio',
+                  type=str, default='Output_REC.slcio')
+parser.add_option('-o', '--outFile', help=f'--outFile {outName}',
+                  type=str, default=outName)
+(options, args) = parser.parse_args()
+
+#Creating output data storage
+#For each doublet layer, the hits in the layer closer to the origin are put into the inner, and hits in the layer farther away is put into the outer
+innerTheta=[]
+innerPhi=[]
+outerTheta=[]
+outerPhi=[]
+deltaTheta=[]
+deltaPhi=[]
+#momentum has one index per event, stores 0 if the particle has decayed, and the truth particles momentum if it has not decayed
+momentum=[]
+
+#Loop over every file
+for f in fnames:
+    reader = IOIMPL.LCFactory.getInstance().createLCReader()
+    reader.open(f)
+
+    #Loop over events in the file
+    for event in reader:
+        #Adding a list to store data for this particular event to each variable
+        momentum.append(0)
+        innerTheta.append([])
+        innerPhi.append([])
+        outerTheta.append([])
+        outerPhi.append([])
+        deltaTheta.append([])
+        deltaPhi.append([])
+        
+        #There are 9 sets of doublet layers, 1 in the barrel, 4 in the negative z endcaps, and 4 in the positive z endcaps
+        #Adding one list per layer. index 0 is the barrel doublet, 1-4 are the -z endcap doubles, and 5-8 are the +z endcap doublets
+        for i in range(9):
+            innerTheta[-1].append(0)
+            innerPhi[-1].append(0)
+            outerTheta[-1].append(0)
+            outerPhi[-1].append(0)
+            deltaTheta[-1].append(0)
+            deltaPhi[-1].append(0)
+
+        #Accessing the collection of all digitized hits in the barrel
+        hitsCollection = event.getCollection("VBTrackerHits")
+        #creating a decoder that will be used layer to trace a hit back to its system and layer
+        encoding=hitsCollection.getParameters().getStringVal(EVENT.LCIO.CellIDEncoding)
+        decoder=UTIL.BitField64(encoding)
+
+        #collections of truth level tracks from mc particles
+        mcTracksCollections=event.getCollection('MCParticle_SiTracks_Refitted')
+
+        for relationsObject in mcTracksCollections:
+
+            #Get the tracks from the relations object. There should only be 1, but I am looping over it incase there is more.
+            for tracks in relationsObject.getTo():
+                
+                #Get the hits from the trcks
+                for hit in tracks.getTrackerHits():
+                    #Decoder to get the location of the hit
+                    cellID = int(hit.getCellID0())
+                    decoder.setValue(cellID)
+                    layer = decoder['layer'].value()
+                    system=decoder["system"].value()
+                    side = decoder["side"].value()
+                    
+                    #finding the hash for the hit so it can be put into the list
+                    #Barrel endcaps
+                    if (system==0):
+                        hash=0
+                    elif (system==1):
+                        hash=1+layer//2+(side==1)*4
+
+                    #inner hits
+                    if layer%2==0:
+                        innerPhi[-1][hash]=hit.getPositionVec().Phi()
+                        innerTheta[-1][hash]=hit.getPositionVec().Theta()
+                    #outer hits
+                    else:
+                        outerPhi[-1][hash]=hit.getPositionVec().Phi()
+                        outerTheta[-1][hash]=hit.getPositionVec().Theta()
+
+            for i in range(9):
+                if (deltaPhi[-1][i] !=0) and (deltaPhi[-1][i] !=0):
+                    deltaPhi[-1][i]=innerPhi[-1][i]-outerPhi[-1][i]
+                    deltaTheta[-1][i]=innerTheta[-1][i]-outerTheta[-1][i]
+
+            #Getting the truth information of the particle that created the track
+            for particle in relationsObject.getFrom():
+                momentum[-1]=particle.getMomentumVec().Pt()
+
+#Wrapping data into a dictionary that will be exported as a json
+output={
+    "deltaTheta" : deltaTheta,
+    "deltaPhi" : deltaPhi,
+    "pt" : momentum
+}
+
+output_json = options.outFile+".json"
+with open(output_json, 'w') as fp:
+    json.dump(output, fp)
