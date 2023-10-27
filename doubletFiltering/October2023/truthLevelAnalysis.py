aee23cff167266de3430fe8eb0f3dfa419b05807
Isaac Hirsch <isaac@isaacs-air-2.lan>
2023-10-26 22:39:29 -0500

    added a search over the hits after the cut was generated

--- doubletFiltering/October2023/truthLevelAnalysis.py
+++ doubletFiltering/October2023/truthLevelAnalysis.py
@@ -4,20 +4,22 @@ from optparse import OptionParser
 import json
 from pyLCIO import IOIMPL, EVENT, UTIL
 
+
+
+#First part of the code finds the cuts for the delta phi and delta theta distributions using noBIB data
+
+
+
+
 #Gather all the files you want to run over.
-#Comment out all but 1 fnames
-#0-50 pt muons
-fnames, outName = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_0_50/muonGun_pT_0_50_reco_*.slcio"), "deltaThetaPhi0NoBIBTruth"
-#50-250 pt muons
-#fnames, outName = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_50_250/muonGun_pT_50_250_reco_*.slcio"), "deltaThetaPhi50NoBIBTruth"
-#250-1000 pt muons
-#fnames, outName = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_250_1000/muonGun_pT_250_1000_reco_*.slcio"), "deltaThetaPhi250NoBIBTruth"
+#0-50 pt muons will be used for the lose fit because they have a realistic beamspot
+fnames= glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_0_50/muonGun_pT_0_50_reco_*.slcio")
 
 parser = OptionParser()
 parser.add_option('-i', '--inFile', help='--inFile Output_REC.slcio',
                   type=str, default='Output_REC.slcio')
-parser.add_option('-o', '--outFile', help=f'--outFile {outName}',
-                  type=str, default=outName)
+parser.add_option('-o', '--outFile', help='--outFile deltaPhiAndThetaLooseCutData',
+                  type=str, default="deltaPhiAndThetaLoseCutData")
 (options, args) = parser.parse_args()
 
 #Creating output data storage
@@ -107,11 +109,145 @@ for f in fnames:
             particle=relationsObject.getFrom()
             momentum[-1]=particle.getMomentumVec().Pt()
 
+phiCut=np.zeros(9)
+thetaCut=np.zeros(9)
+deltaPhiArray=np.array(deltaPhi)
+deltaThetaArray=np.array(deltaTheta)
+for i in range(9):
+    phiCut[i]=np.percentile(deltaPhiArray[(deltaPhiArray[:,i]!=0) & (momentum >=1),i], 99.7)
+    thetaCut[i]=np.percentile(deltaThetaArray[(deltaThetaArray[:,i]!=0) & (momentum >=1),i], 99.7)
+
+
+
+### Second part of the code uses these cuts to find their effects on the BIB data
+
+
+
+#Creating output data storage
+phiDistribution=[]
+thetaDistribution=[]
+hitSurvivalPhi=np.zeros(9)
+hitSurvivalTheta=np.zeros(9)
+hitSurvivalTotal=np.zeros(9)
+for i in range(9):
+    phiDistribution.append([])
+    thetaDistribution.append([])
+
+#Gathering all the BIB files for which we will analyze the effects of the cuts
+fnamesBIB= glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/recoBIB/muonGun_pT_0_50/muonGun_pT_0_50_reco_*.slcio")
+
+#Looping over all the BIB files
+for f in fnamesBIB:
+    reader = IOIMPL.LCFactory.getInstance().createLCReader()
+    reader.open(f)
+
+    #Loop over events in the file
+    for event in reader:
+        
+        #Accessing the collection of all digitized hits in the barrel
+        VBHitsCollection = event.getCollection("VBTrackerHits")
+
+        #Accessing the collection of all digitized hits in the endcaps
+        VEHitsCollection = event.getCollection("VETrackerHits")
+
+        #creating a decoder that will be used layer to trace a hit back to its system and layer
+        encoding=hitsCollection.getParameters().getStringVal(EVENT.LCIO.CellIDEncoding)
+        decoder=UTIL.BitField64(encoding)
+
+        #collections of truth level tracks from mc particles
+        mcTracksCollections=event.getCollection('MCParticle_SiTracks_Refitted')
+
+        #Requiring that the particle has a momentum greater than 1 GeV
+        particle=([relationsObject for relationsObject in mcTracksCollections])[0].getFrom()
+        if particle.getMomentumVec().Pt() < 1:
+            continue
+        
+        #Looping over the objects in the collection of truth level tracks
+        for relationsObject in mcTracksCollections:
+            
+            #Get the tracks from the relations object.
+            track=relationsObject.getTo()
+
+            #Creating a list to store the truth level phi and theta of the hits.
+            #Hash is the same as before but even if the hit is on the inner layer and odd if its on the outer layer of a doublet
+            truthPhi=np.zeros(18)
+            truthTheta=np.zeros(18)
+
+            #Get the hits from the trcks
+            for hit in track.getTrackerHits():
+                #Decoder to get the location of the hit
+                cellID = int(hit.getCellID0())
+                decoder.setValue(cellID)
+                layer = decoder['layer'].value()
+                system=decoder["system"].value()
+                side = decoder["side"].value()
+
+                #finding the hash for the hit so it can be put into the list
+                #Barrel endcaps
+                if (system==1):
+                    hash=0
+                elif (system==2):
+                    hash=1+layer//2+(side==1)*4
+                
+                truthPhi[hash*2+layer%2]=hit.getPositionVec().Phi()
+                truthTheta[hash*2+layer%2]=hit.getPositionVec().Theta()
+
+            for num, hit in enumerate(VBHitsCollection):
+                #Decoder to get the location of the hit
+                cellID = int(hit.getCellID0())
+                decoder.setValue(cellID)
+                layer = decoder['layer'].value()
+
+                if layer==0:
+                    if np.abs(hit.getPositionVec().Phi()-truthPhi[1])<phiCut[0]:
+                        phiDistribution[0].append(hit.getPositionVec().Phi()-truthPhi[1])
+                        hitSurvivalPhi[0]+=1
+                        if np.abs(hit.getPositionVec().Theta()-truthTheta[1])<thetaCut[0]:
+                            hitSurvivalTotal[0]+=1
+
+                    if np.abs(hit.getPositionVec().Theta()-truthTheta[1])<thetaCut[0]:
+                        thetaDistribution[0].append(hit.getPositionVec().Theta()-truthTheta[1])
+                        hitSurvivalTheta[0]+=1
+            
+            hitSurvivalTotal[0]=hitSurvivalTotal[0]/(num+1)
+            hitSurvivalPhi[0]=hitSurvivalPhi[0]/(num+1)
+            hitSurvivalTheta[0]=hitSurvivalTheta[0]/(num+1)
+
+            for num, hit in enumerate(VEHitsCollection):
+                #Decoder to get the location of the hit
+                cellID = int(hit.getCellID0())
+                decoder.setValue(cellID)
+                layer = decoder['layer'].value()
+                side = decoder["side"].value()
+
+                if layer % 2 == 0:
+                    if np.abs(hit.getPositionVec().Phi()-truthPhi[1+layer+(side==1)*8])<phiCut[1+layer//2+(side==1)*4]:
+                        phiDistribution[1+layer//2+(side==1)*4].append(hit.getPositionVec().Phi()-truthPhi[1+layer+(side==1)*8])
+                        hitSurvivalPhi[1+layer//2+(side==1)*4]+=1
+                        if np.abs(hit.getPositionVec().Theta()-truthTheta[1+layer+(side==1)*8])<thetaCut[1+layer//2+(side==1)*4]:
+                            hitSurvivalTotal[1+layer//2+(side==1)*4]+=1
+
+                    if np.abs(hit.getPositionVec().Theta()-truthTheta[1+layer+(side==1)*8])<thetaCut[1+layer//2+(side==1)*4]:
+                        thetaDistribution[1+layer//2+(side==1)*4].append(hit.getPositionVec().Theta()-truthTheta[1+layer+(side==1)*8])
+                        hitSurvivalTheta[1+layer//2+(side==1)*4]+=1
+
+            hitSurvivalTotal[1+layer//2+(side==1)*4]=hitSurvivalTotal[1+layer//2+(side==1)*4]/(num+1)
+            hitSurvivalPhi[1+layer//2+(side==1)*4]=hitSurvivalPhi[1+layer//2+(side==1)*4]/(num+1)
+            hitSurvivalTheta[1+layer//2+(side==1)*4]=hitSurvivalTheta[1+layer//2+(side==1)*4]/(num+1)
+
+
 #Wrapping data into a dictionary that will be exported as a json
 output={
-    "deltaTheta" : deltaTheta,
-    "deltaPhi" : deltaPhi,
-    "pt" : momentum
+    "truthDeltaTheta" : deltaTheta,
+    "truthDeltaPhi" : deltaPhi,
+    "truthPt" : momentum,
+    "hitSurvivalPhi" : hitSurvivalPhi,
+    "hitSurvivalTheta" : hitSurvivalTheta,
+    "hitSurvivalTotal" : hitSurvivalTotal,
+    "phiCut" : phiCut,
+    "thetaCut" : thetaCut,
+    "phiDistribution" : phiDistribution,
+    "thetaDistribution" : thetaDistribution
 }
 
 output_json = options.outFile+".json"
